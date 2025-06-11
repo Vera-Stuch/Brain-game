74c4613f5b24959d9a0aa72b9700a9264f91c2b9
Vera-Stuch <Stuchilinavera@gmail.com>
2025-06-11 14:22:16 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const char HIDDEN_SYMBOL = '#';
+const int MAX_PAIRS = 50;
+
+void playMatchSound();
+void playNoMatchSound();
+
+vector<vector<char>> createBoard(int pairs);
+void printBoard(const vector<vector<char>>& board, const vector<vector<bool>>& revealed);
+
+void getCardCoords(int& row, int& col,
+    const vector<vector<bool>>& revealed,
+    const vector<vector<char>>& board,
+    const string& prompt,
+    int prevRow = -1, int prevCol = -1);
+
+void playGame(int pairs, int playerCount);
